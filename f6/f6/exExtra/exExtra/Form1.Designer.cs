8de2e9e7c24f6f9a8dcc0d317e26aa828965b9f0
Lao Chung <goncalopcbg1@gmail.com>
2026-03-13 16:06:24 +0000

    FULL FEITO A ULTIMA ALINIA FULL GPT

---
+++ f6/f6/exExtra/exExtra/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace exExtra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblModelo = new System.Windows.Forms.Label();
+            this.lblAutonomia = new System.Windows.Forms.Label();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.cbbAutonomia = new System.Windows.Forms.ComboBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.grbHD = new System.Windows.Forms.GroupBox();
+            this.rdbN = new System.Windows.Forms.RadioButton();
+            this.rdbS = new System.Windows.Forms.RadioButton();
+            this.lstbInsc = new System.Windows.Forms.ListBox();
+            this.btnElim = new System.Windows.Forms.Button();
+            this.btnMedia = new System.Windows.Forms.Button();
+            this.grbHD.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblModelo
+            // 
+            this.lblModelo.AutoSize = true;
+            this.lblModelo.Location = new System.Drawing.Point(9, 7);
+            this.lblModelo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblModelo.Name = "lblModelo";
+            this.lblModelo.Size = new System.Drawing.Size(42, 13);
+            this.lblModelo.TabIndex = 0;
+            this.lblModelo.Text = "Modelo";
+            // 
+            // lblAutonomia
+            // 
+            this.lblAutonomia.AutoSize = true;
+            this.lblAutonomia.Location = new System.Drawing.Point(7, 66);
+            this.lblAutonomia.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAutonomia.Name = "lblAutonomia";
+            this.lblAutonomia.Size = new System.Drawing.Size(82, 13);
+            this.lblAutonomia.TabIndex = 1;
+            this.lblAutonomia.Text = "Autonomia (min)";
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(9, 23);
+            this.txtModelo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtModelo.MaxLength = 128;
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(76, 20);
+            this.txtModelo.TabIndex = 2;
+            this.txtModelo.TextChanged += new System.EventHandler(this.txtModelo_TextChanged);
+            // 
+            // cbbAutonomia
+            // 
+            this.cbbAutonomia.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbAutonomia.FormattingEnabled = true;
+            this.cbbAutonomia.Items.AddRange(new object[] {
+            "30",
+            "60",
+            "180",
+            "240",
+            "300"});
+            this.cbbAutonomia.Location = new System.Drawing.Point(9, 81);
+            this.cbbAutonomia.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbbAutonomia.Name = "cbbAutonomia";
+            this.cbbAutonomia.Size = new System.Drawing.Size(92, 21);
+            this.cbbAutonomia.TabIndex = 3;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Enabled = false;
+            this.btnInserir.Location = new System.Drawing.Point(128, 82);
+            this.btnInserir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(56, 19);
+            this.btnInserir.TabIndex = 4;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // grbHD
+            // 
+            this.grbHD.Controls.Add(this.rdbN);
+            this.grbHD.Controls.Add(this.rdbS);
+            this.grbHD.Location = new System.Drawing.Point(209, 13);
+            this.grbHD.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.grbHD.Name = "grbHD";
+            this.grbHD.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.grbHD.Size = new System.Drawing.Size(150, 88);
+            this.grbHD.TabIndex = 5;
+            this.grbHD.TabStop = false;
+            this.grbHD.Text = "Camera HD";
+            // 
+            // rdbN
+            // 
+            this.rdbN.AutoSize = true;
+            this.rdbN.Location = new System.Drawing.Point(13, 50);
+            this.rdbN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rdbN.Name = "rdbN";
+            this.rdbN.Size = new System.Drawing.Size(45, 17);
+            this.rdbN.TabIndex = 1;
+            this.rdbN.Text = "Não";
+            this.rdbN.UseVisualStyleBackColor = true;
+            // 
+            // rdbS
+            // 
+            this.rdbS.AutoSize = true;
+            this.rdbS.Checked = true;
+            this.rdbS.Location = new System.Drawing.Point(13, 28);
+            this.rdbS.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rdbS.Name = "rdbS";
+            this.rdbS.Size = new System.Drawing.Size(44, 17);
+            this.rdbS.TabIndex = 0;
+            this.rdbS.TabStop = true;
+            this.rdbS.Text = "SIM";
+            this.rdbS.UseVisualStyleBackColor = true;
+            // 
+            // lstbInsc
+            // 
+            this.lstbInsc.FormattingEnabled = true;
+            this.lstbInsc.Location = new System.Drawing.Point(9, 124);
+            this.lstbInsc.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lstbInsc.Name = "lstbInsc";
+            this.lstbInsc.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstbInsc.Size = new System.Drawing.Size(351, 186);
+            this.lstbInsc.TabIndex = 2;
+            this.lstbInsc.SelectedIndexChanged += new System.EventHandler(this.lstbInsc_SelectedIndexChanged);
+            // 
+            // btnElim
+            // 
+            this.btnElim.Enabled = false;
+            this.btnElim.Location = new System.Drawing.Point(11, 328);
+            this.btnElim.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnElim.Name = "btnElim";
+            this.btnElim.Size = new System.Drawing.Size(56, 19);
+            this.btnElim.TabIndex = 6;
+            this.btnElim.Text = "Eliminar";
+            this.btnElim.UseVisualStyleBackColor = true;
+            this.btnElim.Click += new System.EventHandler(this.btnElim_Click);
+            // 
+            // btnMedia
+            // 
+            this.btnMedia.Location = new System.Drawing.Point(295, 328);
+            this.btnMedia.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMedia.Name = "btnMedia";
+            this.btnMedia.Size = new System.Drawing.Size(56, 19);
+            this.btnMedia.TabIndex = 7;
+            this.btnMedia.Text = "Média";
+            this.btnMedia.UseVisualStyleBackColor = true;
+            this.btnMedia.Click += new System.EventHandler(this.btnMedia_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 366);
+            this.Controls.Add(this.btnMedia);
+            this.Controls.Add(this.btnElim);
+            this.Controls.Add(this.lstbInsc);
+            this.Controls.Add(this.grbHD);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.cbbAutonomia);
+            this.Controls.Add(this.txtModelo);
+            this.Controls.Add(this.lblAutonomia);
+            this.Controls.Add(this.lblModelo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Drones";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grbHD.ResumeLayout(false);
+            this.grbHD.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblModelo;
+        private System.Windows.Forms.Label lblAutonomia;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.ComboBox cbbAutonomia;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.GroupBox grbHD;
+        private System.Windows.Forms.RadioButton rdbN;
+        private System.Windows.Forms.RadioButton rdbS;
+        private System.Windows.Forms.ListBox lstbInsc;
+        private System.Windows.Forms.Button btnElim;
+        private System.Windows.Forms.Button btnMedia;
+    }
+}
+
---
+++ f6/f6/exExtra/exExtra/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace exExtra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbbAutonomia.SelectedIndex = 3;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void btnInserir_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtModelo_TextChanged(object sender, EventArgs e)
+        {
+            if (txtModelo.Text != "")
+
+                btnInserir.Enabled = true;
+            else
+                btnInserir.Enabled = false;
+        }
+
+        private void btnInserir_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            string linhaFinal = "";
+            string nome = txtModelo.Text;
+            string autonomia = cbbAutonomia.Text;
+
+            if (rdbS.Checked)
+            {
+                linhaFinal = $"{nome}.HD, ({autonomia})";
+
+            }
+            else
+            {
+                linhaFinal = $"{nome}, ({autonomia})";
+
+            }
+            lstbInsc.Items.Add(linhaFinal);
+            txtModelo.Text = "";
+            rdbS.Checked = true;
+            cbbAutonomia.SelectedIndex = 3;
+
+
+        }
+
+
+        private void lstbInsc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstbInsc.SelectedItems.Count > 0)
+                btnElim.Enabled = true;
+            else
+                btnElim.Enabled = false;
+
+        }
+
+        private void btnElim_Click(object sender, EventArgs e)
+        {
+            foreach (object item in lstbInsc.SelectedItems.Cast<object>().ToList())
+            {
+                
+                if (!item.ToString().Contains(".HD"))
+                {
+                    lstbInsc.Items.Remove(item);
+                }
+            }
+            btnElim.Enabled= false;
+
+
+
+
+        }
+
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            double somaHD = 0, somaSemHD = 0, somaGlobal = 0;
+            int contHD = 0, contSemHD = 0, contGlobal = 0;
+
+            foreach (object item in lstbInsc.Items)
+            {
+                string linha = item.ToString();
+
+                // Extrai o valor entre os parênteses (ex: "20min" -> "20")
+                int inicio = linha.IndexOf('(') + 1;
+                int fim = linha.IndexOf(')');
+                string valorTexto = linha.Substring(inicio, fim - inicio).Replace("min", "");
+                double autonomia = double.Parse(valorTexto);
+
+                // Soma para a média global
+                somaGlobal += autonomia;
+                contGlobal++;
+
+                // Separa por tipo (HD ou não)
+                if (linha.Contains(".HD"))
+                {
+                    somaHD += autonomia;
+                    contHD++;
+                }
+                else
+                {
+                    somaSemHD += autonomia;
+                    contSemHD++;
+                }
+            }
+
+            // Calcula as médias (evitando divisão por zero)
+            double mediaGlobal = contGlobal > 0 ? somaGlobal / contGlobal : 0;
+            double mediaHD = contHD > 0 ? somaHD / contHD : 0;
+            double mediaSemHD = contSemHD > 0 ? somaSemHD / contSemHD : 0;
+
+            // Exibe os resultados
+            MessageBox.Show($"Média Global: {mediaGlobal:F2} min\n" +
+                            $"Média com HD: {mediaHD:F2} min\n" +
+                            $"Média sem HD: {mediaSemHD:F2} min");
+            Application.Exit();
+        }
+    }
+}
